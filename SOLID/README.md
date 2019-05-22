74ff9dc83efbb00e95a589e3257e95aaaf7e42b8
matias pavez <matias.pavez.b@gmail.com>
2019-05-23 01:21:54 +0200

    interface segregation

--- SOLID/README.md
+++ SOLID/README.md
@@ -1,33 +1,37 @@
 # SOLID Design Principles
 
-## Overview
-
-- Useful principles of Obejct Oriented Design.
-- Introduced by Uncle Bob (Robert C. Martin)
+Useful principles of Obejct Oriented Design. These were introduced by Uncle Bob (Robert C. Martin).
 
 ## The SOLID Principles
 
-### Single Responsibility Principle
+### Single Responsibility
 
 **Separation of Concerns**: A class should have a single reason to change. 
 
-### Open-Closed Principle
+### Open-Closed
 
-The system should be open to extension and closed to modification.
+The system should be **open to extension and closed to modification**.
 
 - We want to avoid modifying code that already works.
 - Extension can be done by inheritance.
 
-### Liskov Substitution Principle
+### Liskov Substitution
 
 Subtypes should be inmediatly substitutable by their base types.
+
 - Methods designed to work on the Base class should keep working
   when a Derived instance is used.
 - Named after Robert Liskov.
 
-### Interface Segregation Principle
+### Interface Segregation
+
+Don't force implementers to code too large interfaces.
+
+- Partial implementers contain dummy methods and extra code.
+- Partial implementers give a wrong API to the user.
 
 
+### Dependency Inversion
 
-### Dependency Inversion Principle
+
 
---
+++ SOLID/interface_segregation/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Document;
+
+/* This interface is OK if all IMachines really provide 
+   print, scan and fax methods. 
+
+   It forces implementers to create dummy methods when 
+   used on a simpler class.
+*/
+struct BadIMachine
+{
+  virtual void print(Document& doc) = 0;
+  virtual void scan(Document& doc) = 0;
+  virtual void fax(Document& doc) = 0;
+};
+
+/* A Scanner should not implement an IMachine interface. 
+- There are 2 options: Leave blank body or throw an exception.
+- It gives the user a wrong understanding of the class. It
+  will think our scanner can also print.
+
+*/
+struct BadScanner: BadIMachine
+{
+  void print(Document &doc) override {
+    throw new logic_error("Not implemented.");
+  }
+
+  void scan(Document &doc) override {
+    // OK
+  }
+
+  void fax(Document &doc) override {
+    throw new logic_error("Not implemented.");
+  }
+};
+
+
+/* Segregated Classes: IPrinter, IScanner and IFax. */
+struct IPrinter { virtual void print(Document& doc) = 0; };
+struct IScanner { virtual void scan(Document& doc)  = 0; };
+struct IFax     { virtual void fax(Document& doc)   = 0; };
+
+/* Printer does not require to implement dummy methods. */
+struct Printer: IPrinter {
+  void print(Document &doc) override {
+    // OK
+  }
+};
+struct Scanner: IScanner {
+  void scan(Document &doc) override {
+    // OK
+  }
+};
+
+/* Complex Machine implements printer and scanner interfaces. */
+struct ComplexMachine: IPrinter, IScanner {
+  void print(Document &doc) override {
+    // OK
+  }
+
+  void scan(Document &doc) override {
+    // OK
+  }
+};
+
+/* Now we can also use a decorator to create a complex machine. */
+struct DecoratedMachine: IPrinter, IScanner
+{
+  IPrinter& printer;
+  IScanner& scanner;
+
+  DecoratedMachine(IPrinter &printer, IScanner &scanner): printer(printer), scanner(scanner) {}
+
+  void print(Document &doc) override {
+    printer.print(doc);
+  }
+
+  void scan(Document &doc) override {
+    scanner.scan(doc);
+  }
+};
+
+
+int main()
+{
+
+  return 0;
+}
